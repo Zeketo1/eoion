c78230aa6095a30e38ae3dc454ae0d80f908d788
zeketo <ikorofrancis24@gmail.com>
2025-01-18 14:12:57 +0100

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Eoion LTD - Building The Future of SaaS</title>
+      <meta name="title" content="Eoion LTD - Building The Future of SaaS" />
+      <meta name="description" content="A tech company focused on building and scaling innovative SaaS products for internal ventures and external clients." />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://eoion-ltd.com/" />
+      <meta property="og:title" content="Eoion LTD - Building The Future of SaaS" />
+      <meta property="og:description" content="A tech company focused on building and scaling innovative SaaS products for internal ventures and external clients." />
+      <meta property="og:image" content="https://eoion-ltd.com/banner.jpg" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://eoion-ltd.com/" />
+      <meta property="twitter:title" content="Eoion LTD - Building The Future of SaaS" />
+      <meta property="twitter:description" content="A tech company focused on building and scaling innovative SaaS products for internal ventures and external clients." />
+      <meta property="twitter:image" content="https://eoion-ltd.com/banner.jpg" />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
